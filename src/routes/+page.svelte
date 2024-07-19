42fb8ba6910fd528c9b463a7be145808aaeb37f7
Vignesh Aithal <60360863+vigneshksaithal@users.noreply.github.com>
2024-07-19 22:00:57 +0530

    Save code snippets and list them

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,7 @@
 
 	let text = ''
 	let res = ''
+	let codeSnippets = []
 
 	onMount(async () => {
 		Highlight.app.addListener(
@@ -11,9 +12,13 @@
 			async (context: HighlightContext) => {
 				console.log('Invoked', context)
 
+				const description = context.suggestion || ''
+				console.log('description', description)
+
 				let ocrText = context.environment.ocrScreenContents
 				text = ocrText || ''
 				res = await getCodeFromOcrtext(text)
+				saveCode(description, res)
 			},
 		)
 	})
@@ -22,8 +27,9 @@
 		const textPrediction = Highlight.inference.getTextPrediction([
 			{
 				role: 'system',
-				content:
-					'You are a helpful assistant that can answer questions about the user. Extract ONLY code from the given text and return it. Remove the backticks at start and end of code.',
+				content: `You are a helpful assistant that can answer questions about the user. 
+					Extract ONLY code from the given text and return it. 
+					Remove the backticks at start and end of code.`,
 			},
 			{
 				role: 'user',
@@ -39,6 +45,27 @@
 
 		return result
 	}
+
+	const saveCode = async (description: string, code: string) => {
+		codeSnippets.push({ description, code })
+		Highlight.appStorage.set('codeSnippets', codeSnippets)
+	}
+
+	const getCodeSnippets = async () => {
+		const codeSnippets = Highlight.appStorage.get('codeSnippets')
+		return codeSnippets
+	}
 </script>
 
-<pre><code class="language-html">{res}</code></pre>
+<pre><code>{res}</code></pre>
+
+{#await getCodeSnippets()}
+	<p>loading</p>
+{:then codeSnippets}
+	{#each codeSnippets as codeSnippet}
+		<p>{codeSnippet.description}</p>
+		<pre><code>{codeSnippet.code}</code></pre>
+	{/each}
+{:catch error}
+	<p>error</p>
+{/await}
