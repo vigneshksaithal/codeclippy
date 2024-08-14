da1d06f684600a8bd9af47aa5ef3214221880f47
Vignesh Aithal <60360863+vigneshksaithal@users.noreply.github.com>
2024-08-14 22:33:58 +0530

    Use basic REST API instead of PocketBase SDK in share page for speed

--- src/routes/share/[id]/+page.ts
+++ src/routes/share/[id]/+page.ts
@@ -1,14 +1,19 @@
 export const prerender = false
 
-import { pb } from '$lib/pocketbase'
+import { PUBLIC_POCKETBASE_URL } from '$env/static/public'
 import type { PageLoad } from './$types'
 
 export const load = (async ({ params }) => {
     const { id } = params
+    const url = `${PUBLIC_POCKETBASE_URL}/api/collections/codes/records/${id}?fields=title,code`
 
-    const { title, code } = await pb.collection('codes').getOne(id, {
-        fields: 'title, code',
-    })
+    const response = await fetch(url)
+    if (!response.ok) {
+        throw new Error('Failed to fetch code data')
+    }
+    const data = await response.json()
+    const { title, code } = data
+
     return {
         title,
         code
