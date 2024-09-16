bebd8719bd8c723af12ea00347771bbd7086ed01
Vignesh Aithal <60360863+vigneshksaithal@users.noreply.github.com>
2024-09-16 12:59:10 +0530

    refactor: Update font styles and weights for better visual consistency

--- src/routes/share/[id]/+page.server.ts
+++ src/routes/share/[id]/+page.server.ts
@@ -2,24 +2,23 @@ import { PUBLIC_POCKETBASE_URL } from '$env/static/public'
 import type { PageServerLoad } from './$types'
 
 export const load = (async ({ params }) => {
-    const { id } = params
-    const { title, code } = await getCodeFromDB(id)
+	const { id } = params
+	const { title, code } = await getCodeFromDB(id)
 
-    return {
-        title,
-        code,
-        url: `${PUBLIC_POCKETBASE_URL}/share/${id}`
-    }
+	return {
+		title,
+		code,
+		url: `${PUBLIC_POCKETBASE_URL}/share/${id}`,
+	}
 }) satisfies PageServerLoad
 
-
 const getCodeFromDB = async (id: string) => {
-    const url = `${PUBLIC_POCKETBASE_URL}/api/collections/codes/records/${id}?fields=title,code`
+	const url = `${PUBLIC_POCKETBASE_URL}/api/collections/codes/records/${id}?fields=title,code`
 
-    const response = await fetch(url)
-    if (!response.ok) {
-        throw new Error('Failed to fetch code data')
-    }
-    const { title, code } = await response.json()
-    return { title, code }
+	const response = await fetch(url)
+	if (!response.ok) {
+		throw new Error('Failed to fetch code data')
+	}
+	const { title, code } = await response.json()
+	return { title, code }
 }
