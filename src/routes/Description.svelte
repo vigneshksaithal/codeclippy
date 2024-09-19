e64f904c6194f28710dc21bb8bfcc5462c9b12cf
Vignesh Aithal <60360863+vigneshksaithal@users.noreply.github.com>
2024-09-19 10:56:00 +0530

    refactor: Update code snippet copying functionality and UI consistency

--- src/routes/Description.svelte
+++ src/routes/Description.svelte
@@ -5,7 +5,7 @@ export let isHighlight = false
 </script>
 
 <section
-	class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-4"
+	class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
 >
 	<Card.Root>
 		<Card.Header class="text-center">
@@ -16,33 +16,31 @@ export let isHighlight = false
 				Capture and share code snippets effortlessly with Highlight.ing
 			</Card.Description>
 		</Card.Header>
-		<Card.Content>
-			<div class="space-y-4">
-				<video autoplay loop muted class="w-full">
-					<source src="/demo.mp4" type="video/mp4" />
-				</video>
-				<div class="p-4 rounded">
-					<h4 class="text-2xl font-semibold mb-4 text-primary">How to use?</h4>
-					<ol class="list-decimal list-inside space-y-4">
-						<li>Copy any code snippet you want to save.</li>
-						<li>
-							Then invoke the Highlight app and select CodeClippy icon and enter
-							or choose the title for the code snippet.
-						</li>
-						<li>
-							Then it will be automatically saved and you can share it with
-							others by clicking on the share button.
-						</li>
-					</ol>
-				</div>
+		<Card.Content class="space-y-4">
+			<video autoplay loop muted class="w-full">
+				<source src="/demo.mp4" type="video/mp4" />
+			</video>
+			<div class="p-4 rounded">
+				<h4 class="text-2xl font-semibold mb-4 text-primary">How to use?</h4>
+				<ol class="list-decimal list-inside space-y-4">
+					<li>Copy any code snippet you want to save.</li>
+					<li>
+						Open the Highlight app, select the CodeClippy icon, and set a title
+						for your snippet.
+					</li>
+					<li>
+						It will save automatically, allowing you to share via the share
+						button.
+					</li>
+				</ol>
 			</div>
 			{#if !isHighlight}
-				<p class="text-red-600 mt-8 text-center">
+				<p class="text-red-400 mt-8 text-center">
 					[WARNING] This app is designed to run inside the Highlight.ing app.
 					<br />
 					<a
 						href="https://highlight.ing"
-						class="text-primary hover:underline"
+						class="text-primary underline"
 						target="_blank"
 					>
 						Learn more about Highlight
